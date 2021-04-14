5f8d550e1d95f7d0dfe2e31c466b751b9b6fd060
Diamux <Damian22070@gmail.com>
2021-04-14 12:25:22 +0200

    Dodano Easter Egga

---
+++ assets/js/oldstyle.js
@@ -0,0 +1,11 @@
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
+}
